10281522d6031a6ba3947a47762441bcd7023ce7
Joshua Fogg <joshuafoggnz@gmail.com>
2017-06-06 16:47:24 +1200

    finished content

--- game.js
+++ game.js
@@ -12,7 +12,7 @@ function bindEventListeners (dots) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
-    dot[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('click', makeBlue)
   }
 }
 
